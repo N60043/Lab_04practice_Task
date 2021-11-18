98df4d3685bf831405815a08525b5ae4df156ff6
Naqah Abbasi <naqashabbasi96@gmail.com>
2021-11-18 07:20:52 -0800

    update

--- src/Database/DbConnection.java
+++ src/Database/DbConnection.java
@@ -1,4 +1,34 @@
 package Database;
 
+import java.sql.*;
+
 public class DbConnection {
+    public void record(String username) throws SQLException {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8080/servletdb", "root", "");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("INSERT INTO user (id, username)\n" +
+                    "VALUES ('', '')");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet getRecords() throws SQLException {
+        ResultSet rs = null;
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8080/servletdb", "root", "");
+            Statement st = con.createStatement();
+            rs = st.executeQuery("select *from user");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("username"));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return rs;
+    }
 }
